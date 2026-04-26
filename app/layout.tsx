a7ac62697860f20b1a927020f02776fcb2cace23
Freddy Hammer <freddy.hammer@gmail.com>
2026-04-26 03:28:42 +0200

    Add Contentsquare tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -17,7 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full antialiased">
-      <body className="min-h-full">{children}</body>
+      <body className="min-h-full">
+        {children}
+        <Script
+          src="https://t.contentsquare.net/uxa/2b523691042fe.js"
+          strategy="afterInteractive"
+        />
+      </body>
     </html>
   );
 }
